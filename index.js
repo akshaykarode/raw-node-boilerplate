fe56ca28e03fa3c9692d1d62a34e15b46775db82
Akshay <akshay.karode@instarem.com>
2019-09-17 17:58:50 +0530

    linter correction in index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require("express")
-const router = express.Router()
-const app = express()
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const expressStatusMonitor=require('express-status-monitor')
+const express = require('express');
+const router = express.Router();
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const expressStatusMonitor=require('express-status-monitor');
 
-const hostname = process.env.HOSTNAME || 'localhost'
-const publicDir = __dirname + '/public'
-const _config = require('./configs/default.json')
+const hostname = process.env.HOSTNAME || 'localhost';
+const publicDir = __dirname + '/public';
+const _config = require('./configs/default.json');
 
-const Logger = require('./modules/infra/logger')
-const CustomError = require('./modules/comman/error')
+const Logger = require('./modules/infra/logger');
+const CustomError = require('./modules/comman/error');
 // var DB = require('./modules/db')
 
 // Global
+/* global config, __basedir */
 config = _config[process.env.NODE_ENV] || _config['development'];
 __basedir = __dirname;
 // Global Ends
@@ -21,38 +22,38 @@ __basedir = __dirname;
 /* -------- App Configs --------  */
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.set('port', (process.env.PORT || 4000))
+app.set('port', (process.env.PORT || 4000));
 
 app.use(function(req, res, next) { // CORS Enable
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'x-access-token,Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
-})
+});
 app.use(methodOverride());
 app.use(bodyParser.json({
-    limit: '100mb'
+  limit: '100mb',
 }));
 app.use(bodyParser.urlencoded({
   extended: true,
-  limit: '100mb'
+  limit: '100mb',
 }));
 
-app.use(Logger(config))
+app.use(Logger(config));
 app.use(express.static(publicDir));
 app.use(expressStatusMonitor());
 /* --------  App Configs Ends --------  */
 
 /* --------  Routes --------  */
-require('./modules/routes.js')(router)
-require('./modules/infra/swagger')(router)
-app.use(router)
+require('./modules/routes.js')(router);
+require('./modules/infra/swagger')(router);
+app.use(router);
 /* --------  Routes Ends --------  */
 
 /* --------  Global Error Handler  --------  */
-app.use(function (err, req, res, next) {
-  res.status(500).send(new CustomError(err))
+app.use(function(err, req, res, next) {
+  res.status(500).send(new CustomError(err));
 });
 /* --------  Global Error Handler Ends --------  */
 
-console.log("Server is listening at http://%s:%s", hostname, app.get('port') , " ENV:",config.envName);
+console.log('Server is listening at http://%s:%s', hostname, app.get('port'), ' ENV:', config.envName);
 app.listen(app.get('port'), hostname);
